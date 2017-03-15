44661cc56bf749d626d44ef4c9aa20d9602f3798
luc-bucher <lucjanbucher@gmail.com>
2017-03-15 12:53:04 +0100

    Jquery in index.php verschoben

--- app/Views/CreateView.php
+++ app/Views/CreateView.php
@@ -56,7 +56,6 @@
     <br>
     <br>
   </form>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script>
   $('#riskStepDropdown').on("change", function(){
                 value = $(this).val();
