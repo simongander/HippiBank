c8cb8acce409694ad1855e463a21c34a6afe63eb
simongander <simongander.00@gmail.com>
2017-03-15 09:23:32 +0100

    Speicherung in DB

--- app/Models/CreateModel.php
+++ app/Models/CreateModel.php
@@ -7,7 +7,22 @@ $Hypo = $pdo->prepare('SELECT * FROM mortgages');
 $Hypo->execute();
 $hypoPackets = $Hypo->fetchAll();
 
-$Risk = $pdo->prepare('SELECT * FROM Risikostufe');
+$Risk = $pdo->prepare('SELECT * FROM risikostufe');
 $Risk->execute();
 $riskSteps = $Risk->fetchAll();
+
+function SaveToDatabase($name, $email, $phone, $riskStep, $hypoPacket, $payback)
+{
+$Save = $pdo->prepare('INSERT INTO verleihe VALUES (:name, :email, :phone, :riskStep, :hypoPacket, :payback)');
+
+$Save->BindParam(':name', $name);
+$Save->BindParam(':email', $email);
+$Save->BindParam(':phone', $phone);
+$Save->BindParam(':riskStep', $riskStep);
+$Save->BindParam(':hypoPacket', $hypoPacket);
+$Save->BindParam(':payback', $payback);
+
+$Save->execute();
+}
+
 ?>
